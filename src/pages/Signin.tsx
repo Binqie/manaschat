fdc382dfe4c63cd48caebc6590179273fe7033ac
Binqie <shamir.beishebaev@icloud.com>
2023-04-25 20:01:19 +0600

    forms validation and request are done

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -3,165 +3,157 @@ import Avatar from '@mui/material/Avatar'
 import Button from '@mui/material/Button'
 import CssBaseline from '@mui/material/CssBaseline'
 import TextField from '@mui/material/TextField'
-import FormControlLabel from '@mui/material/FormControlLabel'
-import Checkbox from '@mui/material/Checkbox'
-import MuiLink from '@mui/material/Link'
 import Paper from '@mui/material/Paper'
 import Box from '@mui/material/Box'
 import Grid from '@mui/material/Grid'
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
 import Typography from '@mui/material/Typography'
-import { createTheme, ThemeProvider } from '@mui/material/styles'
+import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai'
 
 import { PUBLIC_ROUTES } from '../constants/consts'
 import { Link } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
+import $api from 'api'
 
-function Copyright(props: any) {
-  return (
-    <Typography
-      variant='body2'
-      color='text.secondary'
-      align='center'
-      {...props}
-    >
-      {'Copyright © '}
-      <MuiLink
-        color='inherit'
-        href='https://mui.com/'
-      >
-        Your Website
-      </MuiLink>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  )
+interface IUser {
+  email: string
+  password: string
 }
 
-const theme = createTheme()
-
 const Signin = () => {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    const data = new FormData(event.currentTarget)
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    })
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({ mode: 'onTouched' })
+
+  const onSubmit = async (data: any) => {
+    console.log(data)
+    const userInfo: IUser = {
+      email: data.email,
+      password: data.password,
+    }
+    console.log(userInfo)
+    const response = await $api.post('/Users/SignIn', userInfo)
+    console.log(response)
   }
 
   return (
-    <ThemeProvider theme={theme}>
+    <Grid
+      container
+      component='main'
+      sx={{ height: '100vh' }}
+    >
+      <CssBaseline />
       <Grid
-        container
-        component='main'
-        sx={{ height: '100vh' }}
+        item
+        xs={false}
+        sm={4}
+        md={7}
+        sx={{
+          backgroundImage: 'url(https://source.unsplash.com/random)',
+          backgroundRepeat: 'no-repeat',
+          backgroundColor: (t) =>
+            t.palette.mode === 'light'
+              ? t.palette.grey[50]
+              : t.palette.grey[900],
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+        }}
+      />
+      <Grid
+        item
+        xs={12}
+        sm={8}
+        md={5}
+        component={Paper}
+        elevation={6}
+        square
+        sx={{
+          display: 'flex',
+          alignItems: 'flex-start',
+          justifyContent: 'center',
+        }}
       >
-        <CssBaseline />
-        <Grid
-          item
-          xs={false}
-          sm={4}
-          md={7}
+        <Box
           sx={{
-            backgroundImage: 'url(https://source.unsplash.com/random)',
-            backgroundRepeat: 'no-repeat',
-            backgroundColor: (t) =>
-              t.palette.mode === 'light'
-                ? t.palette.grey[50]
-                : t.palette.grey[900],
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
+            my: 8,
+            mx: 4,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            marginTop: '150px',
+            maxWidth: 400,
           }}
-        />
-        <Grid
-          item
-          xs={12}
-          sm={8}
-          md={5}
-          component={Paper}
-          elevation={6}
-          square
         >
-          <Box
-            sx={{
-              my: 8,
-              mx: 4,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography
+            component='h1'
+            variant='h5'
           >
-            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography
-              component='h1'
-              variant='h5'
+            Sign in
+          </Typography>
+          <Box
+            component='form'
+            noValidate
+            onSubmit={handleSubmit(onSubmit)}
+            sx={{ mt: 1 }}
+          >
+            <TextField
+              margin='normal'
+              required
+              fullWidth
+              id='email'
+              label='Email Address'
+              autoComplete='email'
+              autoFocus
+              size='small'
+              {...(errors.email && {
+                  helperText: "Your email must be in format 0000.00000@manas.edu.kg",
+                })}
+              {...register('email', { required: true, pattern: new RegExp(/[0-9]+\.[0-9]+(@manas\.edu\.kg)$/gm) })}
             >
-              Sign in
-            </Typography>
-            <Box
-              component='form'
-              noValidate
-              onSubmit={handleSubmit}
-              sx={{ mt: 1 }}
+              <AiFillEyeInvisible />
+            </TextField>
+            <TextField
+              margin='normal'
+              required
+              fullWidth
+              label='Password'
+              type='password'
+              id='password'
+              autoComplete='current-password'
+              size='small'
+              {...register('password', { required: true })}
+            />
+            <Button
+              type='submit'
+              fullWidth
+              variant='contained'
+              sx={{ mt: 3, mb: 2 }}
             >
-              <TextField
-                margin='normal'
-                required
-                fullWidth
-                id='email'
-                label='Email Address'
-                name='email'
-                autoComplete='email'
-                autoFocus
-              />
-              <TextField
-                margin='normal'
-                required
-                fullWidth
-                name='password'
-                label='Password'
-                type='password'
-                id='password'
-                autoComplete='current-password'
-              />
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    value='remember'
-                    color='primary'
-                  />
-                }
-                label='Remember me'
-              />
-              <Button
-                type='submit'
-                fullWidth
-                variant='contained'
-                sx={{ mt: 3, mb: 2 }}
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid
+                item
+                xs
               >
-                Sign In
-              </Button>
-              <Grid container>
-                <Grid
-                  item
-                  xs
-                >
-                  <Link to='/password/forgot'>Forgot password?</Link>
-                </Grid>
-                <Grid item>
-                  <Link to={PUBLIC_ROUTES.SIGNUP}>
-                    {"Don't have an account? Sign Up"}
-                  </Link>
-                </Grid>
+                <Link to='/password/forgot'>Forgot password?</Link>
               </Grid>
-              <Copyright sx={{ mt: 5 }} />
-            </Box>
+              <Grid item>
+                <Link to={PUBLIC_ROUTES.SIGNUP}>
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
           </Box>
-        </Grid>
+        </Box>
       </Grid>
-    </ThemeProvider>
+    </Grid>
   )
 }
 
