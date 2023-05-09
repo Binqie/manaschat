9a0d1d2718ad7ba3636b1ff2978c31f4ac0ef9d2
Binqie <shamir.beishebaev@icloud.com>
2023-05-09 22:29:19 +0600

    RTK Query added

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,10 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
 import UserSlice from './slices/UserSlice'
+import { postsApi } from './api/postApi'
 
 export const store = configureStore({
   reducer: {
     user: UserSlice,
+    [postsApi.reducerPath]: postsApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(postsApi.middleware),
 })
 
 export type RootState = ReturnType<typeof store.getState>
