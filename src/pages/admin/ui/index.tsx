a75f4ac0522def90441932730592b84c581c9036
Binqie <shamir.beishebaev@icloud.com>
2023-05-09 22:30:56 +0600

    admin page added

---
+++ src/pages/admin/ui/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainContainer from 'widgets/mainContainer/ui'
+
+const Admin = () => {
+  return (
+    <MainContainer>
+      <div>Admin</div>
+    </MainContainer>
+  )
+}
+
+export default Admin
