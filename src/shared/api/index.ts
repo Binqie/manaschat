fe5d8bbefc88d635f39be1ae5e0984a00d853a38
Binqie <shamir.beishebaev@icloud.com>
2023-05-11 22:11:56 +0600

    axios instance modified

--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -1,14 +1,27 @@
 import axios from 'axios'
 import { BASE_URL } from '../config/consts'
+import Cookies from 'js-cookie'
 
-const $api = axios.create({
+export const $authApi = axios.create({
   baseURL: BASE_URL,
+  withCredentials: true,
   headers: {
     common: {
       'Content-Type': 'application/json;charset=utf-8',
-      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Origin': 'https://localhost:3000',
+      'Access-Control-Expose-Headers': ' X-My-Custom-Header, Set-Cookie',
     },
   },
 })
 
-export default $api
+export const $api = axios.create({
+  baseURL: BASE_URL,
+  withCredentials: true,
+  headers: {
+    common: {
+      'Content-Type': 'application/json;charset=utf-8',
+      'Access-Control-Allow-Origin': 'https://localhost:3000',
+      // 'Access-Control-Expose-Headers': ' X-My-Custom-Header, Set-Cookie',
+    },
+  },
+})
